716885ea8098d8bffb02d0f17563c9ee932a9b16
AlexBarmen <www.lad282@gmail.com>
2024-05-10 23:49:56 +0300

    added changes to models

--- www/userdata/admin.py
+++ www/userdata/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import UserDataModel, UserProgrammingLanguages
+
+
+admin.site.register(UserDataModel)
+admin.site.register(UserProgrammingLanguages)
--- www/userdata/models.py
+++ www/userdata/models.py
@@ -6,7 +6,7 @@ from .validators import validate_fio_format, validate_email, validate_date
 
 
 class UserProgrammingLanguages(models.Model):
-    LANGUAGES_CHOICES = [
+	LANGUAGES_CHOICES = [
         ('PHP', 'PHP'),
         ('Python', 'Python'),
         ('C', 'C'),
@@ -15,7 +15,13 @@ class UserProgrammingLanguages(models.Model):
         ('JS/TS', 'JS/TS'),
         ('Rust', 'Rust'),
     ]
-    programming_languages = models.CharField(max_length=255, choices=LANGUAGES_CHOICES)
+	programming_languages = models.CharField(max_length=255, verbose_name='Языки программирования')
+
+	class Meta:
+		verbose_name = 'Любимые языки программирования'
+
+	def __str__(self):
+		return f'{self.programming_languages}'
 
 
 class UserDataModel(models.Model):
@@ -24,15 +30,18 @@ class UserDataModel(models.Model):
         ('F', 'Female'),
     ]
 
-	fio = models.CharField(max_length=255, validators=[validate_fio_format])
-	phone = models.CharField(max_length=20, validators=[RegexValidator(regex=r'^\d{10}$', message='Введите 10-значный номер телефона')])
-	email = models.EmailField(max_length=100, validators=[validate_email])
-	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	birth = models.DateField(validators=[validate_date]) 
-	user_langs = models.ManyToManyField(UserProgrammingLanguages)
-	biography = models.TextField()
+	fio = models.CharField(max_length=255, validators=[validate_fio_format], verbose_name='ФИО')
+	phone = models.CharField(max_length=20, validators=[RegexValidator(regex=r'^\d{11}$', message='Введите 10-значный номер телефона')],
+						  verbose_name='Номер телефона')
+	email = models.EmailField(max_length=100, validators=[validate_email], verbose_name='Электронная почта')
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='Пол')
+	birth = models.DateField(validators=[validate_date], verbose_name='Дата рождения') 
+	user_langs = models.ManyToManyField(UserProgrammingLanguages, verbose_name='Любимые языки программирования')
+	biography = models.TextField(verbose_name='Биография')
 
 	def __str__(self):
 		return f'{self.fio}, {self.phone}, {self.email}'
 
+	class Meta:
+		verbose_name = 'Данные пользователя'
 
